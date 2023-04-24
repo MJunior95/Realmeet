d7a6e4e275a1b160c743244ca2ed8cdab183544f
Michel Junior <jrcassunde95@gmail.com>
2023-04-24 20:22:03 -0300

    implementando testes unitarios

---
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomMapperTest.java
@@ -0,0 +1,36 @@
+package br.com.sw2you.realmeet.unit;
+
+import static br.com.sw2you.realmeet.domain.entity.Room.newBuilder;
+import static br.com.sw2you.realmeet.utils.MapperUtils.roomMapper;
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.com.sw2you.realmeet.core.BaseUnitTest;
+import br.com.sw2you.realmeet.domain.entity.Room;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
+import br.com.sw2you.realmeet.utils.TestConstants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+class RoomMapperTest extends BaseUnitTest {
+
+    private RoomMapper victim;
+
+    @BeforeEach
+    void setupEach() {
+        victim = roomMapper();
+    }
+
+    @Test
+    void testFromEntityToDto() {
+        var room = newBuilder().id(DEFAULT_ROOM_ID).build();
+        var dto = victim.fromEntityToDto(room);
+
+        assertEquals(room.getName(), dto.getName());
+        assertEquals(room.getSeats(), dto.getSeats());
+        assertEquals(room.getId(), dto.getId());
+    }
+
+}
---
+++ service/src/test/java/br/com/sw2you/realmeet/unit/RoomServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.sw2you.realmeet.unit;
+
+import static br.com.sw2you.realmeet.domain.entity.Room.newBuilder;
+import static br.com.sw2you.realmeet.utils.MapperUtils.roomMapper;
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.roomBuilder;
+import static java.util.Optional.of;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import br.com.sw2you.realmeet.core.BaseUnitTest;
+import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.mapper.RoomMapper;
+import br.com.sw2you.realmeet.service.RoomService;
+import br.com.sw2you.realmeet.utils.MapperUtils;
+import br.com.sw2you.realmeet.utils.TestConstants;
+import br.com.sw2you.realmeet.utils.TestDataCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+class RoomServiceTest extends BaseUnitTest {
+
+    private RoomService victim;
+
+    @Mock
+    private RoomRepository repository;
+
+    @BeforeEach
+    void setupEach() {
+
+        victim = new RoomService(repository, roomMapper());
+    }
+
+    @Test
+    void testGetRoom() {
+        var room = roomBuilder().id(DEFAULT_ROOM_ID).build();
+        when(repository.findById(DEFAULT_ROOM_ID)).thenReturn(of(room));
+        var dto = victim.getRoom(DEFAULT_ROOM_ID);
+
+        assertEquals(room.getName(), dto.getName());
+        assertEquals(room.getSeats(), dto.getSeats());
+        assertEquals(room.getId(), dto.getId());
+
+    }
+
+}
