6354ca9e1ec7d1debae9e5d2dd7c95c17266fce1
Michel Junior <jrcassunde95@gmail.com>
2023-04-24 20:52:55 -0300

    implementando testes de integracao

---
+++ service/src/test/java/br/com/sw2you/realmeet/integration/RoomApiIntegrationTest.java
@@ -0,0 +1,45 @@
+package br.com.sw2you.realmeet.integration;
+
+import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_ID;
+import static br.com.sw2you.realmeet.utils.TestDataCreator.roomBuilder;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import br.com.sw2you.realmeet.api.facade.RoomApi;
+import br.com.sw2you.realmeet.core.BaseIntegrationTest;
+import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.utils.TestConstants;
+import br.com.sw2you.realmeet.utils.TestDataCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class RoomApiIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private RoomApi roomApi;
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Override
+    protected void setupEach() throws Exception {
+        setLocalHostBasePath(roomApi.getApiClient(), "/v1");
+    }
+
+    @Test
+    void testGetRoomSuccess() {
+        var room = roomBuilder().build();
+        roomRepository.saveAndFlush(room);
+
+        assertNotNull(room.getId());
+        assertTrue(room.getActive());
+
+        var dto = roomApi.getRoom(DEFAULT_ROOM_ID);
+
+        assertEquals(room.getName(), dto.getName());
+        assertEquals(room.getSeats(), dto.getSeats());
+        assertEquals(room.getId(), dto.getId());
+
+    }
+}
