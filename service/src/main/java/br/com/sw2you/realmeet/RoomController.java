810da04b521499a4b42b4f8aeeb26c8883e8a40e
Michel Junior <jrcassunde95@gmail.com>
2023-04-24 16:54:24 -0300

    implementando chamada do service e RoomMapper

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -3,9 +3,12 @@ package br.com.sw2you.realmeet;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import br.com.sw2you.realmeet.api.facade.RoomsApi;
-import br.com.sw2you.realmeet.api.model.Room;
+import br.com.sw2you.realmeet.api.model.RoomDTO;
+import br.com.sw2you.realmeet.domain.entity.Room;
 import java.util.concurrent.CompletableFuture;
 import javax.validation.Valid;
+
+import br.com.sw2you.realmeet.service.RoomService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RoomController implements RoomsApi {
 
-    @Override
-    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
-        return supplyAsync(() -> ResponseEntity.ok(new Room().id(1L).name("Room #1")));
+    private final RoomService roomService;
+
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
     }
+
+    @Override
+    public CompletableFuture<ResponseEntity<RoomDTO>> getRoom(Long id) {
+        return supplyAsync(() -> ResponseEntity.ok(roomService.getRoom(id)));
+    }
+
 }
