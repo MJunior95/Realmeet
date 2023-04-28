124e506d582ed9ea713dd9aaf43efd95d1210b9d
Michel Junior <jrcassunde95@gmail.com>
2023-04-28 17:52:36 -0300

    Criando metodos comuns de validaçcao

---
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorConstants.java
@@ -0,0 +1,9 @@
+package br.com.sw2you.realmeet.validator;
+
+public final class ValidatorConstants {
+
+    public static final String MISSING = ".missing";
+    public static final String MAX_LENGTH = ".exceedsMaxLength";
+
+    public ValidatorConstants() {}
+}
--- service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
+++ service/src/main/java/br/com/sw2you/realmeet/validator/ValidatorUtils.java
@@ -1,6 +1,14 @@
 package br.com.sw2you.realmeet.validator;
 
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.MISSING;
+import static br.com.sw2you.realmeet.validator.ValidatorConstants.MAX_LENGTH;
+import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import java.util.Objects;
+
 import br.com.sw2you.realmeet.exception.InvalidRequestException;
+import org.apache.commons.lang3.StringUtils;
 
 public final class ValidatorUtils {
 
@@ -10,5 +18,27 @@ public final class ValidatorUtils {
         if(validationErrors.hasError()){
             throw new InvalidRequestException(validationErrors);
         }
+    }
+
+    public static boolean validateRequired(String field, String fieldName, ValidationErrors validationErrors){
+        if(isBlank(fieldName)){
+            validationErrors.add(fieldName, fieldName + MISSING);
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateRequired(Object field, String fieldName, ValidationErrors validationErrors){
+        if(isNull(fieldName)){
+            validationErrors.add(fieldName, fieldName + MISSING);
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateMaxLength(String field, String fieldName, int maxLength, ValidationErrors validationErrors){
+        if(!isBlank(fieldName) && field.trim().length() > maxLength){
+            validationErrors.add(fieldName, fieldName + MAX_LENGTH);
+            return false;
+        }
+        return true;
     }
 }
